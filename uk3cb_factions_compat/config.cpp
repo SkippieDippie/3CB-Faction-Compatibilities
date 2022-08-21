07ca9a930f81f3f511e8a6bfbe48a5f0542a17ca
SkippieDippie <skippiedipie@gmail.com>
2022-08-21 02:17:42 +0200

    Added the Github repository link to CfgPatches

--- uk3cb_factions_compat/config.cpp
+++ uk3cb_factions_compat/config.cpp
@@ -2,7 +2,7 @@ class CfgPatches {
 	class uk3cb_factions_compat {
 		author = "Skippie";
 		name = "UK3CB Factions - Compatibilities";
-		url = "";
+		url = "https://github.com/SkippieDippie/3CB-Faction-Compatibilities";
 
 		requiredAddons[] = {};
 		requiredVersion = 1.0;
